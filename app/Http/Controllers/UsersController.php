22d276170701f09cb6d0cb45e97bcceb8f1e5850
jason <405339556@qq.com>
2021-03-22 17:22:10 +0800

    完成用户相关功能

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Handlers\ImageUploadHandler;
+use App\Models\User;
+use App\Http\Requests\UserRequest;
+
+class UsersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
+    {
+        $this->authorize('update', $user);
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            } else {
+                //上传有错误  withErrors可以携带回错误信
+                return back()->withErrors(['上传图片格式只支持png, jpg, gif, jpeg这四种格式']);
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
+}
