89d6a2fd652427d82237e9ab496a9218c0a6e705
jason <405339556@qq.com>
2021-03-24 11:51:29 +0800

    完成帖子列表功能

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -34,6 +34,17 @@
 
     </div>
 
+    {{-- 用户发布的内容 --}}
+    <div class="card">
+        <div class="card-body">
+            <ul class="nav nav-tabs">
+                <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+            </ul>
+            @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+        </div>
+    </div>
+
     <div class="layui-container">
         <div class="layui-row layui-col-space15">
             <div class="layui-col-md6 fly-home-jie">
